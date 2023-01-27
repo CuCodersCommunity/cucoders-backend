717024b97b48217cc86bb6282bcc016fecfbcb41
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-01-27 16:35:17 -0500

    creating is member endpoint

---
+++ src/pages/api/members/is-member.js
@@ -0,0 +1,26 @@
+import { Octokit } from "octokit";
+
+export async function get(params) {
+  const octokit = new Octokit({
+    auth: import.meta.env.GITHUB_TOKEN,
+  });
+
+  const username = params.url.searchParams.get("username");
+  var is_member = true;
+
+  const response = await octokit
+    .request("GET /orgs/cucoderscommunity/members/" + username, {
+      org: "ORG",
+      username: username,
+    })
+    .catch((e) => {
+      is_member = false;
+    });
+
+  return new Response(JSON.stringify({ is_member: is_member }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
