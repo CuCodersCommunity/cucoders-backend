683976b521604b58786dbda1c3c9b3a442f93537
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-02-01 11:52:40 -0500

    adding get member endpoint

---
+++ src/pages/api/members/get-member.js
@@ -0,0 +1,35 @@
+import { Octokit } from "octokit";
+
+export async function get(params) {
+  const octokit = new Octokit({
+    auth: import.meta.env.GITHUB_TOKEN,
+  });
+
+  const username = params.url.searchParams.get("username");
+  var error = false;
+  const response = await octokit
+    .request("GET /users/" + username, {
+      username: username,
+    })
+    .catch((e) => {
+      error = true;
+    });
+
+  if (error) {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const data = await response.data;
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
