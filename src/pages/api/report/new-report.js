0efbc4edc5a5521431bdb7f11b3896ec67cd078d
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2025-07-12 18:41:30 -0400

    Update new-report.js

--- src/pages/api/report/new-report.js
+++ src/pages/api/report/new-report.js
@@ -5,13 +5,20 @@ export async function post({ request }) {
 
   const botToken = import.meta.env.TELEGRAM_API_TOKEN;
   const chatId = import.meta.env.TELEGRAM_REPORT_CHAT_ID;
+  const topicId = import.meta.env.TELEGRAM_TOPIC_ID;
   const text = encodeURIComponent(`
 ${data.description}
 
 ${data.url}
   `);
-  const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${text}&parse_mode=markdown`;
-  const response = await axios.get(telegramUrl);
+  const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+const response = await axios.post(telegramUrl, {
+  chat_id: chatId, // ID del grupo
+  text: text,
+  parse_mode: 'Markdown',
+  message_thread_id: topicId 
+});
 
   return new Response(JSON.stringify(response.data), {
     status: 200,
